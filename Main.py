bab581d164cdb59edfbb0426cde1838e2a1d990b
Sarun Maharjan <pikachuran61@gmail.com>
2024-12-30 00:05:33 +0545

    Add files via upload
    Just messing around nothing concrete yet

--- Main.py
+++ Main.py
@@ -1,88 +1,86 @@
-wheel = {
-    "a":1,
-    "b":2,
-    "c":3,
-    "d":4,
-    "e":5,
-    "f":6,
-    "g":7,
-    "h":8,
-    "i":9,
-    "j":10,
-    "k":11,
-    "l":12,
-    "m":13,
-    "n":14,
-    "o":15,
-    "p":16,
-    "q":17,
-    "r":18,
-    "s":19,
-    "t":20,
-    "u":21,
-    "v":22,
-    "w":23,
-    "x":24,
-    "y":25,
-    "z":26
-}
+#Hope this works
+alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-reverse_wheel = {
-    1:"a",
-    2:"b",
-    3:"c",
-    4:"d",
-    5:"e",
-    6:"f",
-    7:"g",
-    8:"h",
-    9:"i",
-    10:"j",
-    11:"k",
-    12:"l",
-    13:"m",
-    14:"n",
-    15:"o",
-    16:"p",
-    17:"q",
-    18:"r",
-    19:"s",
-    20:"t",
-    21:"u",
-    22:"v",
-    23:"w",
-    24:"x",
-    25:"y",
-    26:"z"
-    }
+#Rotor details
+first_rotor = "LUSHQOXDMZNAIKFREPCYBWVGTJ"
+second_rotor = "ZOUESYDKFWPCIQXHMVBLGNJRAT"
+third_rotor = "EHRVXGAOBQUSIMZFLYNWKTPDJC"
+
+
+#for words in word_switch:
+#    alphabets = alphabets.replace(words[0],"-")
+#    alphabets = alphabets.replace(words[1],words[0])
+#    alphabets = alphabets.replace("-",words[1])
+
+i = 0
+j = 0
+
+def first(text):
+    global i
+    global j
+    global first_rotor
+    first_text=""
+    for word in text:
+        a = alphabets.index(word)
+        first_text = first_text + first_rotor[a]
+        i+=1    
+        if i == 26:
+            i = 0
+            j += 1
+            first_alphabet_second_rotor = second_rotor[0]
+            second_rotor = second_rotor.replace(first_alphabet_second_rotor,"")
+            second_rotor = second_rotor + first_alphabet_second_rotor
+        first_alphabet_first_rotor = first_rotor[0]
+        first_rotor = first_rotor.replace(first_alphabet_first_rotor,"")
+        first_rotor = first_rotor + first_alphabet_first_rotor
+        print(first_rotor)
+    return first_text
+
+def second(text):
+    global j
+    global second_rotor
+    second_text = ""
+    for word in text:
+        j+=1
+        a = alphabets.index(word)
+    j += 1
+    return second_text
+
+def third(text):
+    global third_rotor
+    third_text = ""
+    for word in text:
+        a = alphabets.index(word)
+        third_text = third_text + third_rotor[a]
+    return third_text
+
+def reflector(text):
+    reflected_text = ""
+    for word in text:
+        a = alphabets.index(word)
+        reflected_text = reflected_text + third_rotor[a]
+    return reflected_text
+
+def word_switch(text):
+    word_switch = [["A","C"],["H","O"],["R","Q"],["G","M"],["V","X"]]
     
-class enigma():
-    def convert(self,text):
-        text = text.replace(" ","")
-        text = text.lower()
-        i=0
-        final_list = []
-        final = ""
+    for words in word_switch:
         for word in text:
-            current_word_count = wheel.get(word)
-            word_to_get = current_word_count+i
-            if i>26:
-                i-=26
-                if word_to_get > 26:
-                    word_to_get = word_to_get -  26
-                    final_list.append(reverse_wheel.get(word_to_get))
-                else:
-                    final_list.append(reverse_wheel.get(word_to_get))
-            else:
-                if word_to_get > 26:
-                    word_to_get = word_to_get -  26
-                    final_list.append(reverse_wheel.get(word_to_get))
-                else:
-                    final_list.append(reverse_wheel.get(word_to_get))                
-            i+=1
-        final = "".join(final_list)
-        return final 
-    
-eg = enigma()
-first = eg.convert("Hello World This is a random")
-print(first)
+            if words[0] == word:
+                text = text.replace(words[0],"-")
+                text = text.replace(words[1],words[0])    
+                text = text.replace("-",words[1])
+                
+    return text
+
+word = "IXAMXHERE" #X was used as space
+
+prep = word_switch(word)
+
+first_final = third(second(first(prep)))
+reflected = reflector(first_final)
+second_final = first(second(third(reflected)))
+final_text = word_switch(second_final)
+
+print(final_text)
+
